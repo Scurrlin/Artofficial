db67f0a784ece511a74251039c0bd72e3b804ce2
Sean Currlin <sean.currlin@gmail.com>
2024-01-18 10:03:19 -0500

    update middleware in postRoutes.js

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -8,6 +8,8 @@ dotenv.config();
 
 const router = express.Router();
 
+router.use(express.json());
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
