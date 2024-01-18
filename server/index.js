0dea1b69add1dc3b6473f634c3ada75444fb1a2c
Sean Currlin <sean.currlin@gmail.com>
2024-01-18 10:24:23 -0500

    more index.js updates

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
@@ -9,25 +9,28 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
+
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
-    res.status(200).json({
-      message: 'Hello from DALL.E!',
-    });
+  res.status(200).json({
+    message: 'Hello from DALL-E!',
   });
-  
-  const startServer = async () => {
-    try {
-      connectDB(process.env.MONGODB_URL);
-      app.listen(8080, () => console.log("Server started on port http://localhost:8080"));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  startServer();
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+
+    app.listen(8080, () => console.log("Server started on port http://localhost:8080"));
+  } catch (error) {
+    console.error('Error starting the server:', error);
+  }
+};
+
+startServer();
