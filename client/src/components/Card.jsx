b03c357859c1218595a98d86851bbe48319db953
Sean Currlin <sean.currlin@gmail.com>
2024-12-01 16:40:33 -0500

    revert lazy load changes

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
 
 import { download } from '../assets';
 import { downloadImage } from '../utils';
 
 const Card = ({ _id, name, prompt, photo }) => (
   <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
-    <LazyLoadImage
-      className="block w-full h-full object-cover rounded-xl"
+    <img
+      className="w-full h-auto object-cover rounded-xl"
       src={photo}
       alt={prompt}
-      effect="blur"
     />
     <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
       <p className="text-white text-sm overflow-y-auto prompt">{prompt}</p>
