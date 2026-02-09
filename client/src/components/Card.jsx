12d8ebe683fb99ae47cd69796115bccebdb9dbfa
Sean Currlin <sean.currlin@gmail.com>
2026-02-08 23:09:21 -0500

    update Card.jsx

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -24,12 +24,7 @@ const Card = ({ _id, name, prompt, photo }) => {
         <p className="text-white text-sm overflow-y-auto prompt">{prompt.length > 70 ? `${prompt.slice(0, 70)}...` : prompt}</p>
 
         <div className="mt-5 flex justify-between items-center gap-2">
-          <div className="flex items-center gap-2">
-            <div className="w-7 h-7 rounded-full bg-green-700 flex justify-center items-center text-white text-xs font-bold">
-              {name[0]}
-            </div>
-            <p className="text-white text-sm">{name}</p>
-          </div>
+          <p className="text-white text-sm">{name}</p>
           <div className="flex items-center gap-2 flex-shrink-0">
             <button
               type="button"
