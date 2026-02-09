8a70908a02fad7d9ea9da0e7ada8157d53a1f4c4
Sean Currlin <sean.currlin@gmail.com>
2026-02-08 22:12:46 -0500

    update favicon and copy icon

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -37,20 +37,7 @@ const Card = ({ _id, name, prompt, photo }) => {
               className="outline-none bg-transparent border-none cursor-pointer"
               title="Copy prompt"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="white"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M16.5 8.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v8.25A2.25 2.25 0 006 16.5h2.25m8.25-8.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-8.25A2.25 2.25 0 017.5 18v-1.5m8.25-8.25h-6a2.25 2.25 0 00-2.25 2.25v6"
-                />
-              </svg>
+              <img src="/copy.svg" alt="Copy prompt" className="w-6 h-6" />
             </button>
             <button
               type="button"
