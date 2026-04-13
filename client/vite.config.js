c2abbd37f8078df325e621f8d66099a85da55cad
Sean Currlin <sean.currlin@gmail.com>
2026-04-13 17:23:35 -0400

    update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'syntax-hl': ['react-syntax-highlighter'],
-          'markdown': ['react-markdown', 'rehype-raw'],
-          'lottie': ['lottie-react'],
-        },
-      },
-    },
-  },
 })
